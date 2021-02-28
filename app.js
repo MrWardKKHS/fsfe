6979a2ba127a9d7732e5469d2d2ff67f969cd042
pianowithalex <59804251+pianowithalex@users.noreply.github.com>
2021-02-28 17:46:20 +1300

    push test

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-
-	res.send('<h1> Yes, yes he is <h1/>');
+	res.sendFile(__dirname + "/index.html");
 });
+
+app.use(express.static('assets'))
 
 app.get("/test", (req, res) => {
 
